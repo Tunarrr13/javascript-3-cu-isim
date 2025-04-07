7a7cb1d58dc763644cb944c4bc58bc6eb43526d2
Tunarrr13 <tunarmemmedli152@gmail.com>
2025-04-07 17:50:59 +0400

    hejeth

---
+++ app.js
@@ -0,0 +1,10 @@
+let stringg = "Tunar"
+let num1 = 13
+let boolean = 10>9
+let string = "beli"
+
+console.log(typeof stringg)
+console.log(typeof num1)
+console.log(typeof boolean)
+console.log(typeof string)
+
